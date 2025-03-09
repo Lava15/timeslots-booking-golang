f8a8319301684a76df25c9ddb1ea7eca0a91bf44
LA <no>
2025-03-09 14:40:47 +0500

    fix connection

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lava15/timeslots-booking-golang
 
 go 1.22.3
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+)
--- internal/db/db.go
+++ internal/db/db.go
@@ -3,6 +3,8 @@ package db
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
@@ -10,12 +12,14 @@ var DB *sql.DB
 func InitDB() {
 	connStr := "user=postgres dbname=booking password=postgres port=5432 sslmode=disable"
 	var err error
-	DB, err = sql.Open("pg", connStr)
+	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
+		fmt.Printf("Error opening database: %q", err)
 		panic(err)
 	}
 	err = DB.Ping()
 	if err != nil {
+		fmt.Printf("Error pinging database: %q", err)
 		panic(err)
 	}
 	fmt.Println("Connected to database")
@@ -26,11 +30,15 @@ func CreateTables() {
 			CREATE TABLE IF NOT EXISTS users (
 				id SERIAL PRIMARY KEY,
 				email TEXT UNIQUE NOT NULL,
-				password TEXT NOT NULL
-				is_admin BOOLEAN DEFAULT FALSE
+				password TEXT NOT NULL,
+				is_admin BOOLEAN DEFAULT FALSE,
+				created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+				updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+				deleted_at TIMESTAMP
 			);
 		`)
 	if err != nil {
+		fmt.Printf("Error creating users table: %q", err)
 		panic(err)
 	}
 	_, err = DB.Exec(`
@@ -38,7 +46,7 @@ func CreateTables() {
 			id SERIAL PRIMARY KEY,
 			name VARCHAR(255) NOT NULL,
 			description TEXT,
-			price DECIMAL(10, 2) NOT NULL
+			price DECIMAL(10, 2) NOT NULL,
 			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 			updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 			deleted_at TIMESTAMP
