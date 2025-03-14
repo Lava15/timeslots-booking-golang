ba7b643131ae1ee0e0ebad9b83829ca49ae6f3ff
LA <no>
2025-03-14 13:27:15 +0500

    env variables setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/lava15/timeslots-booking-golang/internal/db"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			slog.Info("No .env file found")
+		} else {
+			slog.Error("Error loading .env file", err)
+		}
+	}
+	PORT := os.Getenv("PORT")
+
 	db.InitDB()
 	r := mux.NewRouter()
 	slog.Info("Starting server...")
 	fmt.Println("Starting server...")
-	http.ListenAndServe(":8080", r)
+	slog.Info("Starting server on port", "port", PORT)
+	if err := http.ListenAndServe(":"+PORT, r); err != nil {
+		slog.Error("Failed to start server", "error", err)
+	}
 }
