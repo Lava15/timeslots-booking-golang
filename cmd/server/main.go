e6f5bc8c266a951cac1cf3fb3bbb3fbb74540e4e
LA <no>
2025-03-14 15:01:54 +0500

    add graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/lava15/timeslots-booking-golang/internal/db"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 
@@ -21,14 +28,31 @@ func main() {
 			slog.Error("Error loading .env file", err)
 		}
 	}
-	PORT := os.Getenv("PORT")
-
-	db.InitDB()
 	r := mux.NewRouter()
-	slog.Info("Starting server...")
-	fmt.Println("Starting server...")
-	slog.Info("Starting server on port", "port", PORT)
-	if err := http.ListenAndServe(":"+PORT, r); err != nil {
-		slog.Error("Failed to start server", "error", err)
+	PORT := os.Getenv("PORT")
+	srv := http.Server{
+		Addr:    ":" + PORT,
+		Handler: r,
 	}
+	db.InitDB()
+
+	go func() {
+		slog.Info("Starting server on port", "port", PORT)
+		if err := srv.ListenAndServe(); err != nil {
+			slog.Error("Failed to start server", "error", err)
+		}
+	}()
+	shutdown, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-shutdown.Done()
+	fmt.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", "error", err)
+	}
+	fmt.Println("Server shutdown successfully")
+	fmt.Println("waiting for all goroutines to finish")
+	wg.Wait()
+	fmt.Println("All goroutines finished")
 }
