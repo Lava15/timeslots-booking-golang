243e811a7a97e7eb56282daef4c407b56569d869
LA <no>
2025-03-18 16:06:23 +0500

    fix

--- config/env.go
+++ config/env.go
@@ -1,14 +1,12 @@
 package config
 
 import (
-	"log/slog"
-
 	"github.com/joho/godotenv"
 )
 
 func Init() {
 	err := godotenv.Load()
 	if err != nil {
-		slog.Info("Error loading .env", err)
+		panic("Error loading .env")
 	}
 }
